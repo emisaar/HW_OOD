71cf4304fd444e68170257c095112601470949d9
emisaar <emisaar0406@users.noreply.github.com>
2021-05-26 22:43:17 -0500

    archivo txt

--- MyGame.h
+++ MyGame.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "Player.h"
 
 using namespace std;
@@ -14,7 +15,41 @@ class MyGame
         int counterEnd = 1;
     public:
         void start();
+        void readFIle();
 };
+
+void readFile()
+{
+    char matriz[50][50];
+    ifstream fp("inputSnake.txt"); // LE ASIGNAS A IFSTREAM LA VARIABLE FP
+
+    for (int j = 0; j < 20; j++) //NO IMPORTA EL LARGO DE LA LECTURA
+    {
+        fp >> matriz[0][j]; // LE ASIGNAS LOS VALORES DE FP A LA MATRIZ
+    }
+
+    //cout << matriz[0][2] << endl;
+
+    char opcion = 'J';
+    while (opcion != 'E')
+    {
+        for (int a = 0; a < 20; a++) //NO IMPORTA EL LARGO DE ARCHIVO PUEDES PONER QUE LEA HASTA 50, PORQUE TU CODIGO CUANDO ENCUENTRA UNA E SE DEBE BREAKEAR
+
+        {
+            opcion = matriz[0][a]; // EJEMPLO:
+            if (opcion == 'C')
+            {                           //          C
+                cout << "HOLA" << endl; //          C
+                                        //          E
+            }
+            else if (opcion == 'E')
+            { //     EN ESTE EJEMPLO HAY 3 CARCATERES EN TU TXT, PERO PUEDES LEERLO HASTA 20 CARACTERES (SI QUIERES) Y NO VA A IMPORTAR PORQUE TU CODIGO CUANDO LEE EL E SE DEBE CERRAR.
+                cout << "ADIOS" << endl;
+                break;
+            }
+        }
+    }
+}
 
 void MyGame::start()
 {
