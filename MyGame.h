b15d79661b7caba785cc78c3cae4cf7f24487f16
Sofia Hernandez <a01655084@itesm.mx>
2021-05-27 20:23:21 -0500

    vamos bien

--- MyGame.h
+++ MyGame.h
@@ -15,6 +15,7 @@ protected:
     char userIn;
     int turn = 1;
     int counterEnd = 1;
+    int mode; // ACTUALIZAR UML
 
 public:
     void start();
@@ -46,143 +47,147 @@ void MyGame::start()
             tiles[i] = 'N';
         }
     }
-    // -------------LECTURA TXT----------------
-    char matriz[14][1];            // ABRES UNA MATRIZ
-    ifstream fp("inputSnake.txt"); // LE ASIGNAS A IFSTREAM LA VARIABLE FP
 
-    for (int j = 0; j < 14; j++) //NO IMPORTA EL LARGO DE LA LECTURA
+    // Menu to choose how to play
+    cout << "Press 1 to play manually or 2 to play with a .txt file: ";
+    cin >> mode;
+
+    // Matrix to save the characters from the txt
+    char matriz[14][1];
+    ifstream fp("inputSnake.txt"); // Reads the file
+
+    for (int j = 0; j < 14; j++)
     {
-        fp >> matriz[0][j]; // LE ASIGNAS LOS VALORES DE FP A LA MATRIZ
+        // The data is saved in their corresponding position
+        fp >> matriz[0][j];
     }
 
-    // ---------------------------------------
-
-    while (turn <= 15) // Maximum number of shifts is 15
+    while (turn <= 15) // It limits to play up to 15 turns
     {
-        // Prints the instruction to start the game
         cout << "Press C to continue next turn or E to end the game: ";
-        // cin >> userIn;
 
-        // ------------
-        for (int num = 0; num < 14; num++)
+        userIn = matriz[0][turn - 1];
+
+        if (userIn == 'C')
         {
-            userIn = matriz[0][num];
-
-            // if ((userIn != 'C') && (userIn != 'E'))
-            // {
-            //     // Check if the option is valid
-            //     cout << "Invalid operation. Please press C to continue next turn or E to end the game: ";
-            //     //cin >> userIn;
-            //     // cout << userIn << endl;
-            // }
-
-            if (userIn == 'C')
+            // The game start
+            if (counterEnd % 2 != 0)
             {
-                // The game start
-                if (counterEnd % 2 != 0)
+                // Player's turn
+                cout << turn << " ";
+                // Player number
+                cout << p1->playerNum() << " ";
+
+                // Tile before throwing
+                cout << p1->getPos(temp1) << " ";
+
+                // Dice number
+                diceP1 = p1->rollDice();
+                cout << diceP1 << " ";
+                // Tile symbol
+                temp1 += diceP1;
+
+                // New position
+                if (tiles[temp1] == 'S')
                 {
-                    // Player's turn
-                    cout << turn << " ";
-                    // Player number
-                    cout << p1->playerNum() << " ";
-
-                    // Tile before throwing
-                    cout << p1->getPos(temp1) << " ";
-
-                    // Dice number
-                    diceP1 = p1->rollDice();
-                    cout << diceP1 << " ";
-                    // Tile symbol
-                    temp1 += diceP1;
-
-                    // New position
-                    if (tiles[temp1] == 'S')
-                    {
-                        //if the tile is a snake the position changes -= 3
-                        cout << tiles[temp1] << " ";
-                        temp1 = p1->snake(temp1);
-                        cout << temp1 << endl;
-                    }
-                    else if (tiles[temp1] == 'L')
-                    {
-                        //if the tile is a ladder the position changes += 3
-                        cout << tiles[temp1] << " ";
-                        temp1 = p1->ladder(temp1);
-                        cout << temp1 << endl;
-                    }
-                    else
-                    {
-                        // else the tile is normal
-                        cout << tiles[temp1] << " " << temp1 << endl;
-                    }
-
-                    // Check position
-                    if (temp1 >= 30)
-                    {
-                        // If the position is 30 or more the player win
-                        cout << "\n-- GAME OVER --" << endl;
-                        cout << "Player 1 is the winner!!!" << endl;
-                        break;
-                    }
+                    //if the tile is a snake the position changes -= 3
+                    cout << tiles[temp1] << " ";
+                    temp1 = p1->snake(temp1);
+                    cout << temp1 << endl;
+                }
+                else if (tiles[temp1] == 'L')
+                {
+                    //if the tile is a ladder the position changes += 3
+                    cout << tiles[temp1] << " ";
+                    temp1 = p1->ladder(temp1);
+                    cout << temp1 << endl;
                 }
                 else
                 {
-                    // Player's turn
-                    cout << turn << " ";
-                    // Player number
-                    cout << "2 ";
-                    // Tile before throwing
-                    cout << p2->getPos(temp2) << " ";
-                    // Dice number
-                    diceP2 = p2->rollDice();
-                    cout << diceP2 << " ";
-                    // Tile symbol
-                    temp2 += diceP2;
-
-                    // New position
-                    if (tiles[temp2] == 'S')
+                    // else the tile is normal
+                    if (temp1 >= 30)
                     {
-                        //if the tile is a snake the position changes -= 3
-                        cout << tiles[temp2] << " ";
-                        temp2 = p2->snake(temp2);
-
-                        cout << temp2 << endl;
-                    }
-                    else if (tiles[temp2] == 'L')
-                    {
-                        //if the tile is a ladder the position changes += 3
-                        cout << tiles[temp2] << " ";
-                        temp2 = p2->ladder(temp2);
-
-                        cout << temp2 << endl;
+                        cout << "N 30" << endl;
                     }
                     else
                     {
-                        // else the tile is normal
-                        cout << tiles[temp2] << " " << temp2 << endl;
+                        cout << tiles[temp1] << " " << temp1 << endl;
                     }
+                }
 
-                    // Check position
-                    if (temp2 >= 30)
-                    {
-                        // If the position is 30 or more the player win
-                        cout << "\n-- GAME OVER --" << endl;
-
-                        cout << "Player 2 is the winner!!!" << endl;
-                        break;
-                    }
-
-                    turn += 1;
+                // Check position
+                if (temp1 >= 30)
+                {
+                    // If the position is 30 or more the player win
+                    cout << "\n-- GAME OVER --" << endl;
+                    cout << "Player 1 is the winner!!!" << endl;
+                    break;
                 }
             }
             else
             {
-                // If the user enters 'E' the game stop
-                cout << "Thanks for playing!!!" << endl;
-                break;
+                // Player's turn
+                cout << turn << " ";
+                // Player number
+                cout << "2 ";
+                // Tile before throwing
+                cout << p2->getPos(temp2) << " ";
+                // Dice number
+                diceP2 = p2->rollDice();
+                cout << diceP2 << " ";
+                // Tile symbol
+                temp2 += diceP2;
+
+                // New position
+                if (tiles[temp2] == 'S')
+                {
+                    //if the tile is a snake the position changes -= 3
+                    cout << tiles[temp2] << " ";
+                    temp2 = p2->snake(temp2);
+
+                    cout << temp2 << endl;
+                }
+                else if (tiles[temp2] == 'L')
+                {
+                    //if the tile is a ladder the position changes += 3
+                    cout << tiles[temp2] << " ";
+                    temp2 = p2->ladder(temp2);
+
+                    cout << temp2 << endl;
+                }
+                else
+                {
+                    // else the tile is normal
+                    if (temp2 >= 30)
+                    {
+                        cout << "N 30" << endl;
+                    }
+                    else
+                    {
+                        cout << tiles[temp2] << " " << temp2 << endl;
+                    }
+                }
+
+                // Check position
+                if (temp2 >= 30)
+                {
+                    // If the position is 30 or more the player win
+                    cout << "\n-- GAME OVER --" << endl;
+
+                    cout << "Player 2 is the winner!!!" << endl;
+                    break;
+                }
+
+                turn += 1;
             }
-            counterEnd += 1;
         }
+        else
+        {
+            // If the user enters 'E' the game stop
+            cout << "Thanks for playing!!!" << endl;
+            break;
+        }
+        counterEnd += 1;
     }
     if (turn >= 15)
     {
